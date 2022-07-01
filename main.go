8f36223557622c7d121497d651c1db4d7729b16d
曾江彬 <zengjiangbin@lingxing.com>
2022-07-01 20:52:47 +0800

    feat: web服务器

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", Welcome)
+	http.ListenAndServe(":80", nil)
+}
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welcome to goblog!")
 }
